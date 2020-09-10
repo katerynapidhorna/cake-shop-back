51990b6d5ab2049d7e7960e15cf1d0b6e73f40fb
katerynapidhorna <katerynapidhorna@gmail.com>
2020-09-10 16:25:16 +0200

    adjust relation cart and order, remove not needed migrations

--- models/cart.js
+++ models/cart.js
@@ -13,9 +13,9 @@ module.exports = (sequelize, DataTypes) => {
         through: "product_cart",
         foreignKey: "cartId",
       });
-      cart.hasMany(models.order, {
-        foreignKey: "cartId"
-      } )
+      cart.belongsTo(models.order, {
+        foreignKey: "cartId",
+      });
     }
   }
   cart.init(
--- models/order.js
+++ models/order.js
@@ -8,8 +8,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      order.belongsTo(models.cart);
-      order.hasOne(models.payment)
+      order.hasOne(models.cart);
+      order.hasOne(models.payment);
     }
   }
   order.init(
